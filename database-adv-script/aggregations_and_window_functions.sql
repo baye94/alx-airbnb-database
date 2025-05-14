e52b8e8af77e559019a3fba0c32875f3b3e1c489
Baye Cheikh FALL <fallcheikh@finetechgroupe.com>
2025-05-14 23:03:01 +0000

    correction

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -5,10 +5,10 @@ SELECT
 FROM bookings
 GROUP BY user_id;
 
--- Rank properties based on total number of bookings using RANK
+-- Rank properties based on total number of bookings using ROW_NUMBER
 SELECT
     property_id,
     COUNT(*) AS total_bookings,
-    RANK() OVER (ORDER BY COUNT(*) DESC) AS booking_rank
+    ROW_NUMBER() OVER (ORDER BY COUNT(*) DESC) AS booking_rank
 FROM bookings
 GROUP BY property_id;
